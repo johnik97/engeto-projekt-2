f3be3bc18fd5da82abe77ff671449fb028c4052b
johnik97 <johnik97@seznam.cz>
2024-01-11 15:21:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuList = document.querySelector('nav');
+const hamburgerIcon = document.querySelector('.fa-solid');
+
+menuIcon.addEventListener('click', () => {
+    if (hamburgerIcon.classList[1] === 'fa-bars') {
+        hamburgerIcon.classList.add('fa-xmark');
+        hamburgerIcon.classList.remove('fa-bars');
+        menuList.style.opacity = '0';
+        menuList.style.display = 'block';
+        setTimeout(() => {
+            menuList.style.transition = 'opacity .25s ease-in-out';
+            menuList.style.opacity = '1';
+        }, 250);
+        document.querySelector('header').classList.add('nav-open');
+    } else {
+        hamburgerIcon.classList.add('fa-bars');
+        hamburgerIcon.classList.remove('fa-xmark');
+        menuList.style.opacity = '0';
+        setTimeout(() => {
+            menuList.style.transition = 'opacity .25s ease-in-out';
+            menuList.style.display = 'none';
+            menuList.style.opacity = '1';
+        }, 250);
+        document.querySelector('header').classList.remove('nav-open');
+    }
+});
